2c72258973bb6f8821ef1e5851ca825b17d06510
Cooper Healy <mhealy@mst.edu>
2018-10-05 12:46:41 -0500

    begin haskell rewrite

--- learner1.hs
+++ learner1.hs
@@ -1,63 +1,64 @@
 {-
-PROCEDURE Incremental_Linear_Learner
-GIVEN:
-E[0..n] : examples, each a <x, y> pair
-eta : the learning rate
-LOCAL:
-    w[0,1] : weights
-Randomize w[0], w[1].
-REPEAT 5000 times
-    FOR k := 0 to n DO                        // go through examples
-        Compute yCap ( from w[0], w[1] and E[k] ) 
-        error :=  Ex[k].y - yCap
-        FOR each weight w[i]
-w[i] := w[i]  +  eta * error * E[k].x[i] 
-         return w[]
+  Programmer: Matthew Healy <mhrh3@mst.edu>
+      Course: CS5001 Deep Learning
+  Assignment: HW #1, Linear Regression Learner
+   Professor: Dr. Ricardo Morales
 -}
+import System.Random
+import System.IO
+import Data.List
+import Data.Char
 
-updateWeight :: Num -> Num -> Num -> Num -> Num
-updateWeight wi eta err xi =
-  wi + eta * err * xi
+sumSquareError :: (Num a) => a -> a -> [(a, a)] -> a
+sumSquareError w0 w1 [es] =
+  sum [ ( ( snd e ) - ( yHat w0 w1 e ) )^2 | e <- es ]
 
-updateWeights :: [Num] -> [Num]
-updateWeights = map updateWeight
+yHat :: (Num a) => [a] -> [a] -> a
+yHat ws xs = let xs' =
+  sum $ [(ws !! i) * (xs !! i) | i <- [1..(len xs)]]
 
-computeYHat :: Num -> Num -> (Num, Num) -> Num
-computeYHat w0 w1 (x, y) = undefined
+linError :: (Num a) => [a] -> ([a] -> a) -> a
+linError ws (xs y) = y - (yHat ws xs)
 
-computeError :: Num -> Num -> Num
-computeError y yh = y - yh
+learn :: (Num a) => [a] -> [a] -> a -> [a]
+learn weights examples eta =
+  learn' weights examples eta 5000
+  where learn' ws es eta count
+          | count == 0 = ws
+          | otherwise = let s = len xs in
+            [(ws !! i) + eta * (linError ws es !! i) * (es !! i) | i <- [1..s]]
 
-learn :: [(Num, Num)] -> Num -> (Num, Num)
-learn examples eta =
-  go examples eta w0 w1 5000
-  where 
-    w0 = rand 0 100
-    w1 = rand 0 100
-    go ex eta w0 w1 count
-          | count <= 0 = (w0, w1)
+getExamples :: String -> [([Num], Num)]
+getExamples s =
+  getExamples' (splitOn "\n" s) []
+  where getExamples' s es
+          | s == [] = es
           | otherwise =
             let
-              yh = [computeYHat w0 w1 x | x <- ex]
-              err = [computeError (snd (ex !! i)) (yh !! i) | i <- [1..(len ex)] ]
+              (xString, yString) 
             in
-              go ex eta (updateWeights ws) (count - 1)
+              [(xs, y)] ++ getExamples' (tail s) es
+
+
 
 main = do
-  let
-    eta = 2
-    (w0, w1) = learn examples eta
-    yh = [computeYHat w0 w1 x | x <- examples]
-    totalError = sum [(examples !! i) - (yh !! i) | i <- [1..(len examples)]]
-    hdr = ("CS-5001: HW#1 \nProgrammer: Matthew Healy <mhrh3>\n\n"
-    ++ "TRAINING\n"
-    ++ "Using random seed = " ++ (show seed) ++ "\n"
-    ++ "Using learning rate eta = " ++ (show eta) ++ "\n"
-    ++ "After 5000 iterations:\n"
-    ++ "Weights:\n"
-    ++ "w0 = " ++ (show w0) ++ "\n"
-    ++ "w1 = " ++ (show w1) ++ "\n"
-    ++ "\n"
-    ++ "VALIDATION\n"
-    ++ "Sum-of-Squares Error = " ++ (show totalError))
-  putStrLn $ hdr
+  contents <- readFile "trash.txt"
+  es = getExamples contents
+  seed = --wat now???
+  eta = 2 -- arbitrarily pick learning rate
+  g <- getStdGen
+  ws = take 2 $ randomRs (0, 100) g -- initialize weights
+  ws' = learn ws es eta
+  sse = sumSquareError ws' es
+  hdr = ("CS-5001: HW#1 \nProgrammer: Matthew Healy <mhrh3>\n\n"
+  ++ "TRAINING\n"
+  ++ "Using random seed = " ++ (show seed) ++ "\n"
+  ++ "Using learning rate eta = " ++ (show eta) ++ "\n"
+  ++ "After 5000 iterations:\n"
+  ++ "Weights:\n"
+  ++ "w0 = " ++ (show ws' !! 0) ++ "\n"
+  ++ "w1 = " ++ (show ws' !! 1) ++ "\n"
+  ++ "\n"
+  ++ "VALIDATION\n"
+  ++ "Sum-of-Squares Error = " ++ (show sse))
+  putStrLn hdr
